6958a8a752148da91dac8b94b4b13f2a87e64198
Rohit Negi <rohitnegi1108@gmail.com>
2026-01-30 01:36:28 +0530

    fix minor issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     <nav className="bg-[rgba(255,255,255,0.3)] fixed top-0 left-0 right-0 z-[100]  ">
       <div className="px-8 sm:px-4 md:px-8 pt-2 ">
         <div className=" mx-auto flex justify-between items-center ">
-          <a href="/" className="z-[60] xl:ml-20 block">
+          <a href="#" className="z-[60] xl:ml-20 block">
             <h1 className="text-4xl text-neutral-950 font-h font-bold tracking-tight leading-none xl:text-5xl">
               ROHIT
               <br />
